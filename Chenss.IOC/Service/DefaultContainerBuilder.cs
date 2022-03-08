d982474007ab9492df51ba0d2f601d5eb326a97b
chenshisheng@aecg.com.cn <chenshisheng@aecg.com.cn>
2022-03-08 13:51:36 +0800

    增加作用域注入

--- Chenss.IOC/Service/DefaultContainerBuilder.cs
+++ Chenss.IOC/Service/DefaultContainerBuilder.cs
@@ -1,10 +1,16 @@
 ﻿using Chenss.IOC.Extensions;
 using Chenss.IOC.IService;
+using System.Collections.Concurrent;
 
 namespace Chenss.IOC.Service
 {
     public class DefaultContainerBuilder : IChenssContainerBuilder
     {
+        /// <summary>
+        /// 作用域字典
+        /// </summary>
+        private ConcurrentDictionary<object, ServiceDescriptorContext> ServiceScopeCollection = new ConcurrentDictionary<object, ServiceDescriptorContext>();
+
         public void RegisterTransient<TService, TImplementation>()
            where TService : class
            where TImplementation : TService
@@ -26,21 +32,62 @@ namespace Chenss.IOC.Service
             TypeLifetime.Singleton.AddRegisterType(typeof(TImplementation), typeof(TService));
         }
 
+
+        public IChenssContainerBuilder CreateScope()
+        {
+            // 实例化作用域容器
+            ServiceScopeCollection = new ConcurrentDictionary<object, ServiceDescriptorContext>();
+            return this;
+        }
+
         public TService Resolve<TService>()
         {
             var context = typeof(TService).FullName.GetDataContext();
-            // 单例校验结果
-            if (context.Lifetime == TypeLifetime.Singleton && context.ImplementationInstance != null)
+            if (ValidateImplInstance<TService>(context))
             {
                 return (TService)context.ImplementationInstance;
             }
             var constructorBinder = new ContextBinder(context);
             var implementationInstance = constructorBinder.Resolve();
-            // 单例实体对象赋值
-            if (context.Lifetime == TypeLifetime.Singleton) 
-                context.ImplementationInstance = implementationInstance;
-
+            SetImplInstance<TService>(context, implementationInstance);
             return (TService)implementationInstance;
         }
+
+        #region 私有方法
+
+        private bool ValidateImplInstance<TService>(ServiceDescriptorContext context)
+        {
+            if (context.Lifetime == TypeLifetime.Singleton && context.ImplementationInstance != null)
+            {
+                return true;
+            }
+            if (context.Lifetime == TypeLifetime.Scoped)
+            {
+                if (ServiceScopeCollection.TryGetValue(typeof(TService).FullName, out var scope))
+                {
+                    if (scope.ImplementationInstance != null)
+                    {
+                        return true;
+                    }
+                }
+                context.ImplementationInstance = null;
+            }
+            return false;
+        }
+
+        private void SetImplInstance<TService>(ServiceDescriptorContext context, object implementationInstance)
+        {
+            if (context.Lifetime == TypeLifetime.Singleton)
+            {
+                context.ImplementationInstance = implementationInstance;
+            }
+            else if (context.Lifetime == TypeLifetime.Scoped)
+            {
+                context.ImplementationInstance = implementationInstance;
+                ServiceScopeCollection.GetOrAdd(typeof(TService).FullName, context);
+            }
+        }
+
+        #endregion
     }
 }
--- Chenss.IOCTest/Program.cs
+++ Chenss.IOCTest/Program.cs
@@ -9,17 +9,25 @@ namespace Chenss.IOCTest
         static void Main(string[] args)
         {
             IChenssContainerBuilder builder = new DefaultContainerBuilder();
-            builder.RegisterTransient<IServiceA, ServiceA>();
-            builder.RegisterTransient<IServiceB, ServiceB>();
+            builder.RegisterSingleton<IServiceA, ServiceA>();
+            builder.RegisterScoped<IServiceB, ServiceB>();
             builder.RegisterTransient<IServiceC, ServiceC>();
             builder.RegisterTransient<IServiceD, ServiceD>();
 
 
+
+
             var a = builder.Resolve<IServiceA>();
             var a1 = builder.Resolve<IServiceA>();
-            var b = builder.Resolve<IServiceB>();
-            var c = builder.Resolve<IServiceC>();
-            var d = builder.Resolve<IServiceD>();
+
+            var container = builder.CreateScope();
+            var a2 = container.Resolve<IServiceA>();
+
+            Console.WriteLine(Object.ReferenceEquals(a, a1));
+            Console.WriteLine(Object.ReferenceEquals(a, a2));
+            //var b = builder.Resolve<IServiceB>();
+            //var c = builder.Resolve<IServiceC>();
+            //var d = builder.Resolve<IServiceD>();
             Console.WriteLine("Hello World!");
         }
     }
